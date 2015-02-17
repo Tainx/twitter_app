e27c011cdb097bcddd218ddb34a9dc61e8db67db
Tainx <quentin.cavalie@epita.fr>
2015-02-17 14:35:38 +0100

    Static Pages done

--- Gemfile
+++ Gemfile
@@ -22,10 +22,11 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
+gem 'spork-rails', :github => 'sporkrb/spork-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
+gem 'redgreen'
+gem 'autotest-notification'
 # Use Unicorn as the app server
 # gem 'unicorn'
 group :development do
@@ -34,7 +35,13 @@ end
 
 group :test do
   gem 'rspec', '3.2.0'
-  gem 'webrat', '0.7.1'
+  gem 'capybara'
+  gem 'ZenTest'
+  gem 'test-unit'
+  gem 'redgreen'
+  gem 'autotest-rails'
+  gem 'autotest'
+  gem 'rnotify'
 end
 
 # Use Capistrano for deployment
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
@@ -16,4 +18,31 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
+
+feature "HomePage" do
+  before { visit '/pages/home'}
+  scenario do
+    expect(page.html).to have_title("Twitter App | Home")
+  end
+end
+
+feature "ContactPage" do
+  before { visit '/pages/contact'}
+  scenario do
+    expect(page.html).to have_title("Twitter App | Contact")
+  end
+end
+
+feature "AboutPage" do
+  before { visit '/pages/about'}
+  scenario do
+    expect(page.html).to have_title("Twitter App | About")
+  end
 end
